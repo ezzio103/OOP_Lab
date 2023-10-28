a995ff549b4df3f26d23dc70f5aed10604f0cce3
ezzio103 <tuancho1003hn@gmail.com>
2023-10-28 19:41:28 +0700

    manage cart

--- Lab2/AimsProject/Aims.java
+++ Lab2/AimsProject/Aims.java
@@ -6,8 +6,18 @@ public class Aims {
             DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion", "Animation","Roger Allers",87,19.95f);
             DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
             DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+			Cart anOrder = new Cart();
+
 
             
+			anOrder.addDigitalVideoDisc(dvd1);
+            anOrder.addDigitalVideoDisc(dvd2);
+			anOrder.addDigitalVideoDisc(dvd3);
+			anOrder.totalCost();
+			anOrder.removeDigitalVideoDisc(dvd2);
+            
+			anOrder.removeDigitalVideoDisc(dvd3);
+            anOrder.totalCost();
         }
     
 }
